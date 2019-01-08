de373cb4c207b84c65c56400938702294ce4f800
andreas <andreas-cooper@hotmail.com>
2019-01-08 10:27:19 +0100

    intet

--- src/moviesystem/GUI/FXMLDocumentController.java
+++ src/moviesystem/GUI/FXMLDocumentController.java
@@ -5,11 +5,15 @@
  */
 package moviesystem.GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
@@ -49,13 +53,17 @@ public class FXMLDocumentController implements Initializable
     }
 
     @FXML
-    private void handleaddcat(ActionEvent event)
+    private void handleaddcat(ActionEvent event) throws IOException
     {
+         
+        
+        
     }
 
     @FXML
     private void handledeletecat(ActionEvent event)
     {
     }
+
     
 }
