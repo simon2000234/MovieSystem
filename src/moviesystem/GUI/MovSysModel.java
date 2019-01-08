f9bc49c45477371b45998bf90360940c4d2195f2
CasperLC <43999061+CasperLC@users.noreply.github.com>
2019-01-08 10:26:53 +0100

    Fixes to category
    AddCategory in gui works+updates list

--- src/moviesystem/GUI/MovSysModel.java
+++ src/moviesystem/GUI/MovSysModel.java
@@ -5,11 +5,52 @@
  */
 package moviesystem.GUI;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import moviesystem.BE.Category;
+import moviesystem.BE.Movie;
+import moviesystem.BLL.MovSysManager;
+
 /**
  *
  * @author Caspe
  */
 public class MovSysModel
 {
-    
+
+    private MovSysManager msm;
+    private ObservableList<Movie> movies;
+    private ObservableList<Category> categories;
+    private Category selectedCategory;
+
+    public MovSysModel()
+    {
+        this.msm = new MovSysManager();
+        this.movies = FXCollections.observableArrayList();
+//        movies.addAll()
+        this.categories = FXCollections.observableArrayList();
+        categories.addAll(msm.getAllCategories());
+    }
+
+    public ObservableList<Category> getCategories()
+    {
+        return categories;
+    }
+
+    public void createCategory(String name)
+    {
+        Category newCategory = msm.createCategory(name);
+        categories.add(newCategory);
+    }
+
+    public Category getSelectedCategory()
+    {
+        return selectedCategory;
+    }
+
+    public void setSelectedCategory(Category selectedCategory)
+    {
+        this.selectedCategory = selectedCategory;
+    }
+
 }
