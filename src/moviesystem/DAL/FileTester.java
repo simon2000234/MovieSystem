ac18513d89a2f707f47583d7d572dae13ed0d9f0
rich0285 <44639744+rich0285@users.noreply.github.com>
2019-01-09 17:34:43 +0100

    navn på film or rating

--- src/moviesystem/DAL/FileTester.java
+++ src/moviesystem/DAL/FileTester.java
@@ -18,19 +18,34 @@ import org.jsoup.select.Elements;
 public class FileTester {
 
     public static void main(String[] args) throws SQLException, IOException {
+        /*
+        Dette viser navn og rating på filmen. Der skal være et link for at det virker, dog også code smells. Richart :)
+         */
         try {
-            Document doc = Jsoup.connect("https://www.imdb.com/title/tt4154756/?ref_=nv_sr_2/").userAgent("Mozilla/17.0").get();
-            Elements temp = doc.select("div.imdbRating");
+            Document doc = Jsoup.connect("https://www.imdb.com/title/tt1825683/?ref_=tt_rec_tti/").userAgent("Mozilla/17.0").get();
+            Elements movTemp = doc.select("div.titleBar");
 
-            for (org.jsoup.nodes.Element ratingValue : temp) {
-
-                System.out.println("" + ratingValue.getElementsByTag("span").first().text());
+            Elements ratTemp = doc.select("div.imdbRating");
+            for (org.jsoup.nodes.Element filmNavn : movTemp) {
+                System.out.println(filmNavn.getElementsByTag("h1").first().text());
 
             }
         } catch (IOException e) {
 
         }
+        {
+            try {
+                Document doc = Jsoup.connect("https://www.imdb.com/title/tt1825683/?ref_=tt_rec_tti/").userAgent("Mozilla/17.0").get();
+                Elements movTemp = doc.select("div.imdbRating");
 
+                for (org.jsoup.nodes.Element ratingValue : movTemp) {
+                    System.out.println("" + ratingValue.getElementsByTag("span").first().text());
+
+                }
+            } catch (IOException e) {
+
+            }
+
+        }
     }
-
 }
