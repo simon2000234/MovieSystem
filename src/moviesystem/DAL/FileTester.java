a307dd2d11084ae48587c22430f2f330188c8c45
rich0285 <44639744+rich0285@users.noreply.github.com>
2019-01-09 14:21:28 +0100

    imdb ratings via imdb links i filetester virker.

--- src/moviesystem/DAL/FileTester.java
+++ src/moviesystem/DAL/FileTester.java
@@ -7,37 +7,30 @@ package moviesystem.DAL;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import javax.lang.model.element.Element;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-            
+
 /**
  *
  * @author Richart hansen
  */
-public class FileTester
-{
+public class FileTester {
 
-    public static void main(String[] args) throws SQLException, IOException
-    {
-       try{
-        Document doc = Jsoup.connect("https://www.imdb.com/title/tt1517451/?ref_=nv_sr_2/").userAgent("Mozilla/17.0").get();
-           Elements temp=doc.select("div.imdbRating");
-           
-           int i= 1;
-           for(org.jsoup.nodes.Element ratingValue:temp)
-           {
-               i++;
-               System.out.println(i+""+ ratingValue.getElementsByTag("span").first().text() );
-                  
-           }
-       }
-       catch (IOException e) 
-       {
-           
-       }
-       
+    public static void main(String[] args) throws SQLException, IOException {
+        try {
+            Document doc = Jsoup.connect("https://www.imdb.com/title/tt4154756/?ref_=nv_sr_2/").userAgent("Mozilla/17.0").get();
+            Elements temp = doc.select("div.imdbRating");
+
+            for (org.jsoup.nodes.Element ratingValue : temp) {
+
+                System.out.println("" + ratingValue.getElementsByTag("span").first().text());
+
+            }
+        } catch (IOException e) {
+
+        }
+
     }
-    
+
 }
