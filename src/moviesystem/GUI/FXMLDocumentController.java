528ab0da83e20ab24a130dafcd905ef20ef8983a
CasperLC <43999061+CasperLC@users.noreply.github.com>
2019-01-11 10:13:49 +0100

    personalRating fix

--- src/moviesystem/GUI/FXMLDocumentController.java
+++ src/moviesystem/GUI/FXMLDocumentController.java
@@ -114,7 +114,7 @@ public class FXMLDocumentController implements Initializable
     @FXML
     private void handledeletemovie(ActionEvent event)
     {
-        msmodel.deleteMovie(msmodel.get);
+//        msmodel.deleteMovie(msmodel.get);
     }
 
     @FXML
